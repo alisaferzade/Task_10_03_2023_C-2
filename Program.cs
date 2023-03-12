d554b2d918c8f93b97ee8db67b911076682f7cf2
Ali Safarli <7n0mn2a@code.edu.az>
2023-03-13 01:29:46 +0400

    Ev tapshirigi C#

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿////Task1
+//Console.Write("Ededi daxil edin : ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//int reverse = 0;
+//while (x != 0)
+//{
+//    reverse = reverse * 10;
+//    reverse += x % 10;
+//    x = (x - x % 10) / 10;
+//}
+//Console.WriteLine(reverse);
+
+//Task2
+//Console.Write("Ededi daxil edin : ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//int eded = x;
+//int count = 0;
+
+//while (x != 0)
+//{
+   
+//    x = (x - x % 10) / 10;
+//    count++;
+
+//}
+//Console.WriteLine(3*Math.Pow(10,count)+eded);
